64a029ccb5e442db5a108dfadbfc6ce373c066c3
Mayank Singh Chauhan <mayanksingh2298@gmail.com>
2017-11-29 00:33:57 +0530

    init

---
+++ bookShelf/books/static/get_papers.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#code').hide();
+	$('#code').fadeIn('slow');
+
+	$('.nav_li').hide();
+	$('.nav_li').fadeIn('slow');
+
+	$('#papers').hide();
+	$('#papers').slideDown('slow');
+});
---
+++ bookShelf/oldUI/static/get_course_codes.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.code').hide();
+	$('.code').slideDown('slow');	
+
+	$('.department').hide();
+	$('.department').fadeIn('slow');	
+
+	$('.course_list').hide();
+	$('.course_list').fadeIn('slow');
+
+	$('.nav_li').hide();
+	$('.nav_li').fadeIn('slow');
+});
