1a3cc4b4d9e6cb4381592a5d58a2dca90441843b
Mayank Singh Chauhan <mayanksingh2298@gmail.com>
2017-11-29 04:36:24 +0530

    ignore

--- bookShelf/bookShelf/settings.py
+++ bookShelf/bookShelf/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -121,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL  = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
--- bookShelf/bookShelf/urls.py
+++ bookShelf/bookShelf/urls.py
@@ -23,3 +23,9 @@ urlpatterns = [
     # url(r'^books/light/', include('books.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
