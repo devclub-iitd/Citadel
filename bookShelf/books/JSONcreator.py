984933db87bc193a1d1f44e56a9d5c745a1cfaea
ankurshaswat <ankurshaswat@yahoo.co.in>
2017-11-29 00:42:10 +0530

    JSON creator added

---
+++ bookShelf/books/JSONcreator.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+def path_to_dict(path):
+    d = {'name': os.path.basename(path)}
+    if os.path.isdir(path):
+        # d['type'] = "directory"
+        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
+(path)]
+    # else:
+        # d['type'] = "file"
+    return d
+
+print(json.dumps(path_to_dict('.')))
