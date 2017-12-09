7b667ea260421fb1b3d82c87910937bf8f939034
ozym4nd145 <ozym4nd145@outlook.com>
2017-12-09 13:41:15 +0530

    Added applicable routing and new view

--- bookShelf/books/JSONcreator.py
+++ bookShelf/books/JSONcreator.py
@@ -2,6 +2,51 @@ import os
 import json
 import collections
 
+class FilePath(Exception):
+    """
+        Raised when the path given is pointing to a file
+        instead of a director
+    """
+    pass
+class InvalidPath(Exception):
+    """
+        Raised when the path given is not a valid heirarchical
+        structure in the database
+    """
+    pass
+
+def navigate_path(db,path):
+    """
+        Returns appropriate dict as pointed to by path.
+        In case of invalid path raise appropriate exceptions
+    """
+    keys = list(filter(None, path.split("/")))
+    for key in keys:
+        try:
+            db = db[key]
+        except Exception as e:
+            raise InvalidPath
+    if type(db) is str:
+        raise FilePath
+    return db
+
+def build_nav_path(prefix,path):
+    """
+        Builds a list of tuple for access links.abs
+        E.g.
+            prefix: /books/view/
+            path: CO/COL100
+            result:
+                [("Home","/books/view"),("CO","/books/view/CO"),("COL100","/books/view/COL100")]
+    """
+    result = [("Home",prefix)]
+    acc = prefix
+    keys = list(filter(None, path.split("/")))
+    for key in keys:
+        acc = os.path.join(acc,key)
+        result.append((key,acc))
+    return result
+
 def generate_path(path):
     d = collections.OrderedDict()
     if os.path.isdir(path):
@@ -11,7 +56,8 @@ def generate_path(path):
                 continue
             d[x]=new_path
     else:
-        return "file"
+        ## TODO: MORE ROBUST PATH CONFIGURATION
+        return path[2:]
     return d
 
 def path_to_dict(path,name_of_file):
