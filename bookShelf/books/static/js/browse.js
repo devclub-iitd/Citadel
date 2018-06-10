7985191be60329f37b8ed30e0e0a927bf237ac44
Kabir <kabirtomer@gmail.com>
2018-06-10 21:32:38 +0530

    Fix UI for tree based storage

--- bookShelf/books/static/js/browse.js
+++ bookShelf/books/static/js/browse.js
@@ -109,20 +109,6 @@ function create_elem_col(path_prefix,name,is_file)
         new_id += path_prefix[i][0]+"/";
     }
 
-    if(is_file){ 
-        //get file data from filename of metafile
-        var desc = name.split("==")
-        if (desc.length==3){
-            if (desc[2]=='.meta'){
-                name = desc[0]
-                var raw_loc = desc[1]
-                var dirs = raw_loc.split('-')
-                var file_dir = dirs.join("/")
-                var file_loc = file_dir + '/' + name
-            }
-        }
-    }
-
     new_id += name+"/";
 
     path_prefix.push([name,"#"+new_id]);
@@ -130,17 +116,16 @@ function create_elem_col(path_prefix,name,is_file)
     var html = '<a href="#" class="list-group-item list-group-item-action col-item-wrap" id="'+new_id+'"><div class="col-item" title="'+name+'">'+name+'</div></a>';
     var btn = $.parseHTML(html)[0];
 
-    var handler = get_event_handler_col(is_file,path_prefix.slice(),file_loc);
+    var handler = get_event_handler_col(is_file,path_prefix.slice(),MEDIA_PREFIX+new_id);
     
     btn.onclick = handler;
     if (path_prefix.length == 2){
         //to add the download course button
-        var url = name;
-        html = '<a href="#" class="list-group-item list-group-item-action col-item-wrap""><div class="col-item" title="jkj">'+"Download Course"+'</div></a>';
+        html = '<a href="#" class="list-group-item list-group-item-action col-item-wrap""><div class="col-item" title="jkj">'+"Download Course Zip"+'</div></a>';
         var bt=$.parseHTML(html)[0];
 
         handler = function(){
-            var win = window.open(ZIP_URL+url,'_blank');
+            var win = window.open(ZIP_URL+name,'_blank');
             win.focus();
         };
         bt.onclick = handler;
