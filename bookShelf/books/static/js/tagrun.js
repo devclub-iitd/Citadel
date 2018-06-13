3e9867d8763f06b6f861a7d4687aa1d1fb9bfb19
Kabir <kabirtomer@gmail.com>
2018-06-13 17:38:39 +0530

    Fix reload tags bug

--- bookShelf/books/static/js/tagrun.js
+++ bookShelf/books/static/js/tagrun.js
@@ -1,17 +1,20 @@
 //run after tagtest.js
 
-tagInput(document)
-
-
+var input= document.getElementsByClassName("fake-input")[0]
 var true_input= document.createElement("input")
+var current_tags_string =document.getElementById("current-tags").value
+var form = document.getElementsByClassName("uploadform")[0]
+
 true_input.id="true_input"
 true_input.setAttribute("name", "tags");
 true_input.setAttribute("type", "hidden");
+input.value=current_tags_string
 
-var form = document.getElementsByClassName("uploadform")[0]
+tagInput(document)
+
 document.getElementsByClassName("submit")[0].addEventListener("click", function(){
-	document.body.appendChild(form)
 	var tagsList = [];
+	var tags = document.getElementsByClassName("tag")
 	for (var i = 0; i < tags.length; i++) {
         tagsList.push(tags[i].getElementsByClassName("tag-text")[0].innerHTML);
 	}
@@ -19,3 +22,17 @@ document.getElementsByClassName("submit")[0].addEventListener("click", function(
 	form.appendChild(true_input);
 	form.submit()
 })
+document.getElementsByClassName("reset")[0].addEventListener("click", function(){
+	
+	form.reset()
+	var tags = document.getElementsByClassName("tag")
+	console.log(tags)
+
+	while(tags.length>0) {
+		tags[0].parentNode.removeChild(tags[0])
+	}
+	input.value=current_tags_string
+	tagInput(document)
+})
+
+
