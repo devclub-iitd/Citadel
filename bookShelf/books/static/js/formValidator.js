be7019273aa464e497c7923eca88a38895a73aee
Kabir <kabirtomer@gmail.com>
2018-06-14 19:15:35 +0530

    Clean up upload UI

--- bookShelf/books/static/js/formValidator.js
+++ bookShelf/books/static/js/formValidator.js
@@ -16,30 +16,30 @@ document.getElementById("customFilename").addEventListener("input", function() {
         document.getElementById("customFilename").setCustomValidity("");
     }
 });
-document.getElementById("upload-btn").addEventListener("click", function() {
+// document.getElementById("upload-btn").addEventListener("click", function() {
 
-    var radios = document.getElementsByName("type_exam");
-    var course_code = document.getElementById("upload-form").elements[1].value;
-    var sem = document.getElementById("upload-form").elements[2].value;
-    var year = document.getElementById("upload-form").elements[3].value;
-    var prof = document.getElementById("upload-form").elements[4].value;
-    var exam = "none";
-    var other_text = document.getElementById("upload-form").elements[11].value;
-    for(var i=0,length=radios.length;i<length;i++) {
-        if (radios[i].checked) {
-            exam = radios[i].value;
-            break;
-        }
-    }
-    if(sem=="" && year=="" && exam!= "other") {
-        document.getElementById("other").setCustomValidity("Other must be selected if Semester and Year are left blank.");
-    } else {
-        document.getElementById("other").setCustomValidity("");
-    }
-    if (exam=="other" && other_text=="") {
-        document.getElementById("customFilename").setCustomValidity("Please enter the file name as well");
-    } else {
-        document.getElementById("customFilename").setCustomValidity("");            
-    }
-});
+//     var radios = document.getElementsByName("type_exam");
+//     var course_code = document.getElementById("upload-form").elements[1].value;
+//     var sem = document.getElementById("upload-form").elements[2].value;
+//     var year = document.getElementById("upload-form").elements[3].value;
+//     var prof = document.getElementById("upload-form").elements[4].value;
+//     var exam = "none";
+//     var other_text = document.getElementById("upload-form").elements[11].value;
+//     for(var i=0,length=radios.length;i<length;i++) {
+//         if (radios[i].checked) {
+//             exam = radios[i].value;
+//             break;
+//         }
+//     }
+//     if(sem=="" && year=="" && exam!= "other") {
+//         document.getElementById("other").setCustomValidity("Other must be selected if Semester and Year are left blank.");
+//     } else {
+//         document.getElementById("other").setCustomValidity("");
+//     }
+//     if (exam=="other" && other_text=="") {
+//         document.getElementById("customFilename").setCustomValidity("Please enter the file name as well");
+//     } else {
+//         document.getElementById("customFilename").setCustomValidity("");            
+//     }
+// });
 
