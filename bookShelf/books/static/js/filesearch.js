a6e84463d8cea992657c3354d2dd762d1b062890
Kabir <kabirtomer@gmail.com>
2018-06-15 14:07:14 +0530

    Make search bars responsive to enter key

--- bookShelf/books/static/js/filesearch.js
+++ bookShelf/books/static/js/filesearch.js
@@ -7,42 +7,52 @@ $(document).ready(function(){
 		$("input")[0].value=''
 	})
 
+	$("input")[0].addEventListener("keydown",function(e){
+        if(e.keyCode === 13 || e.which ===13){
+            e.preventDefault(); 
+            file_search();
+        }
+    })
+
 	$("button")[0].addEventListener("click", function(){
-		DB={};;                        
-		$.getJSON( '/books/api/search',{"path":"/","query":$("input")[0].value}, function( data ){
-			// var DB={};;
-		    DBZ=data;
-		    $.getJSON( API_URL,{"path":"/",depth:DEPTH}, function( data ){
-		        DB=data;	    
-		        // reset output
-		        reset_results()
-
-				for (var i=0; i< DBZ["result"].length;i++){
-
-			    	var path_prefixer=DBZ["result"][i]
-			    	var l =  path_prefixer.length
-
-			    	if (typeof(get_prefix_dict(path_prefixer))==="string"){
-
-				    	var name =path_prefixer[l-1][0]
-					  	
-					    var display_path=''
-					    console.log(path_prefixer)
-					    for (var j=0;j<l-1;j++){
-					    	display_path+= path_prefixer[j][0]+'/'
-					    }
-					    display_path+=name
-					    var html = "<div class='file-name file-box'><a href="+MEDIA_PREFIX+display_path+" target = '_blank'>"+display_path+"</a></div>"
-					    var link = $.parseHTML(html)
-					    
-				        $("#output").append(link)
-				        console.log(link)
-					}
-			    }
-			});
-		});
+		file_search()
 	})
 })
+
+
+function file_search(){
+	DB={};;                        
+	$.getJSON( '/books/api/search',{"path":"/","query":$("input")[0].value}, function( data ){
+		// var DB={};;
+	    DBZ=data;
+	    $.getJSON( API_URL,{"path":"/",depth:DEPTH}, function( data ){
+	        DB=data;	    
+	        // reset output
+	        reset_results()
+
+			for (var i=0; i< DBZ["result"].length;i++){
+
+		    	var path_prefixer=DBZ["result"][i]
+		    	var l =  path_prefixer.length
+
+		    	if (typeof(get_prefix_dict(path_prefixer))==="string"){
+
+			    	var name =path_prefixer[l-1][0]
+				  	
+				    var display_path=''
+				    for (var j=0;j<l-1;j++){
+				    	display_path+= path_prefixer[j][0]+'/'
+				    }
+				    display_path+=name
+				    var html = "<div class='file-name file-box'><a href="+MEDIA_PREFIX+display_path+" target = '_blank'>"+display_path+"</a></div>"
+				    var link = $.parseHTML(html)
+				    
+			        $("#output").append(link)
+				}
+		    }
+		});
+	});
+}
 
 function reset_results(){
 	var box = $.parseHTML("<div id='output' class='results-box'></div>")
--- bookShelf/books/static/js/searchtest.js
+++ bookShelf/books/static/js/searchtest.js
@@ -2,42 +2,9 @@
 //only run after browse.js
 $(document).ready(function(){
 	$("button")[0].addEventListener("click", function(){
-		if($("input")[0].value != ''){
-			DB={};
-			update_view([]);                    
-			redraw_path_bar([["Home","#"]]);
-	
-			$.getJSON( '/books/api/search',{"path":"/","query":$("input")[0].value}, function( data ){
-				// var DB={};;
-			     DBZ=data;
-			    $.getJSON( API_URL,{"path":"/",depth:DEPTH}, function( data ){
-			        DB=data;	    
-					for (var i=0; i< DBZ["result"].length;i++){
-				    	var path_prefixer=DBZ["result"][i]
-				    	path_prefixer[0].push("#")
-	
-	
-				    	// indicate difference in depth of path_prefix and depth of divs to display
-				    	// workaround; unneeded if frontend is redesigned
-				    	var l =  path_prefixer.length
-				    	path_prefixer[0].push(l)
-				    	if (typeof(get_prefix_dict(path_prefixer))==="string"){
-				    		var is_file=true;
-				    	}
-				    	else{
-				    		var is_file=false;
-				    	}
-				        $(".list-group").append(create_elem_col(path_prefixer.slice(0,l-1),path_prefixer[l-1][0],is_file))
-				    }
-				});
-			});
-		};
+		search()
 	})
-})
 
-
-
-$(document).ready(function(){
 
 	$("button")[1].addEventListener("click", function(){
         $.getJSON( API_URL,{"path":"/",depth:DEPTH}, function( data )
@@ -45,7 +12,47 @@ $(document).ready(function(){
             var DB=data;                        
             update_view([]);                    
             redraw_path_bar([["Home","#"]]);
+            $("input")[0].value= ''
         })
     });
+
+    $("input")[0].addEventListener("keydown",function(e){
+        if(e.keyCode === 13 || e.which ===13){
+            e.preventDefault(); 
+            search();
+        }
+    })
 })
 
+function search(){
+	if($("input")[0].value != ''){
+		DB={};
+		update_view([]);                    
+		redraw_path_bar([["Home","#"]]);
+
+		$.getJSON( '/books/api/search',{"path":"/","query":$("input")[0].value}, function( data ){
+			// var DB={};;
+		     DBZ=data;
+		    $.getJSON( API_URL,{"path":"/",depth:DEPTH}, function( data ){
+		        DB=data;	    
+				for (var i=0; i< DBZ["result"].length;i++){
+			    	var path_prefixer=DBZ["result"][i]
+			    	path_prefixer[0].push("#")
+
+
+			    	// indicate difference in depth of path_prefix and depth of divs to display
+			    	// workaround; unneeded if frontend is redesigned
+			    	var l =  path_prefixer.length
+			    	path_prefixer[0].push(l)
+			    	if (typeof(get_prefix_dict(path_prefixer))==="string"){
+			    		var is_file=true;
+			    	}
+			    	else{
+			    		var is_file=false;
+			    	}
+			        $(".list-group").append(create_elem_col(path_prefixer.slice(0,l-1),path_prefixer[l-1][0],is_file))
+			    }
+			});
+		});
+	};
+};
