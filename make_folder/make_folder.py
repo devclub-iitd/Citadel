0d7e40a56f03795a1e9dcaab758b8857814d409e
Kabir <kabirtomer@gmail.com>
2018-09-05 16:44:50 +0530

    Fix dept not defined error

--- make_folder/make_folder.py
+++ make_folder/make_folder.py
@@ -22,6 +22,7 @@ if not os.path.exists('DATA'):
 for course in courses:
     course_start = course[:2]
 
+    dept = course_start
     if not os.path.exists('DATA/'+dept):
         os.makedirs('DATA/'+dept)
     if not os.path.exists('DATA/'+dept+'/'+course):
