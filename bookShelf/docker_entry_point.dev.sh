202095dba1989c8d436f3fb3000ecde534af6375
Shashwat Shivam <shivamshaswat@gmail.com>
2020-08-07 17:59:18 +0530

    Added docker dev server scripts

---
+++ bookShelf/docker_entry_point.dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Dev Script starts"
+
+until psql $DATABASE_URL -c '\l'; do
+	>&2 echo "Postgres is unavailable - sleeping"
+	sleep 1
+done
+
+python manage.py makemigrations books
+python manage.py migrate
+python manage.py crontab add
+python manage.py collectstatic --noinput
+#move to .env
+./citadel_superuser.sh
+echo "Starting Dev WEB Server"
+gunicorn bookShelf.wsgi:application --bind 0.0.0.0:7000 --workers 3
+
+echo "Script complete"
+
+exec "$@"
