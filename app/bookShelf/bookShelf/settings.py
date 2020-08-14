0e16d6a8c5e8a5d4e38e82e27091194b71009b4b
Divyansh <divi09112@gmail.com>
2020-08-14 17:26:19 +0530

    turn off debug

--- app/bookShelf/bookShelf/settings.py
+++ app/bookShelf/bookShelf/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=9l%a%_$j91fchb(bq180=4^=tsdz4&79aw4@_tlhw1a4ctjsj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','*']
 
--- app/bookShelf/docker_entry_point.sh
+++ app/bookShelf/docker_entry_point.sh
@@ -14,7 +14,8 @@ python manage.py collectstatic --noinput
 #move to .env
 ./citadel_superuser.sh
 echo "Starting WEB Server"
-gunicorn bookShelf.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --forwarded-allow-ips="10.17.6.85"
+gunicorn bookShelf.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --forwarded-allow-ips="10.17.6.85" \
+    --access-logfile - --error-logfile -
 
 echo "Script complete"
 
